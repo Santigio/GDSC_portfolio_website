0e0ad46e2551ff1c07d980bad85abc7a17d5562b
Santigio <santigiesankoh233@gmail.com>
2022-12-22 16:19:45 +0200

    new charges

--- starter code/script.js
+++ starter code/script.js
@@ -1,13 +1,3 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelectorAll(".nav-link");
-
-navToggle.addEventListener("click", () => {
-  document.body.classList.toggle("nav-open");
-});
-
-navLinks.forEach(link => {
-  link.addEventListener("click", () => {
-    document.body.classList.remove("nav-open");
-  });
-});
-
+window.onload = function () {
+  alert("Welcome to my website!");
+}
